ecc00c1ae49223bc9efac59be4d78c584ca207f2
dy <df.creative@gmail.com>
2020-10-28 14:45:10 -0400

    Implement basic refs

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 class ElementTemplate extends HTMLTemplateElement {
   constructor() {
     super()
-  }
-  connectedCallback() {
+
     // children are added immediately after the element, so we init as fast as content is ready via MO
     const mo = new MutationObserver(() => {
-      this.#init()
+      // FIXME: find first non script/style node
+      const element = this.content.firstElementChild;
+      const ids = this.content.querySelectorAll('[id]')
+
+      let init
+      const script = this.content.querySelector('script');
+      if (script.type === 'module') {
+        // FIXME: top-level module exports (can be supported by native declarative custom-elements, some service-workers hack etc)
+        // see https://2ality.com/2019/10/eval-via-import.html
+      }
+      else if (script.hasAttribute('scoped')) {
+        init = new Function(`refs`, `with(refs){${script.innerHTML}}`)
+      }
+
+      const style = this.content.querySelector('style')
+
+      let ex = element.getAttribute('is'), Root = ex ? document.createElement(ex).constructor : HTMLElement
+
+      // define custom element
+      customElements.define(element.localName, class extends Root {
+        #refs = {}
+        constructor() {
+          super()
+          ids.forEach(({id}) => Object.defineProperty(this.#refs, id, { get: () => this.querySelector('#' + id) }))
+        }
+        connectedCallback() {
+          // prerender defined children before actual adding to DOM
+          this.append(...element.cloneNode(true).childNodes)
+
+          init.call(this, this.#refs)
+          // FIXME: add refs when child content is parsed
+        }
+      }, ex ? {extends: ex} : null)
+
       mo.disconnect()
     })
-    // unfortunately we cannot observe `this`, because template does not get notified after parsing children, so we wait for any other mutation
-    mo.observe(this.parentNode, {childList: true, subtree: true})
-  }
-
-  #init() {
-    // FIXME: find first non script/style node
-    this.orig = this.content.firstElementChild
-
-    // FIXME: detect extension case
-    // define custom element
-    customElements.define(this.orig.localName, class extends HTMLElement {
-      constructor() {
-        super()
-      }
-    })
+    mo.observe(this.content, {childList: true, subtree: true})
   }
 }
 
